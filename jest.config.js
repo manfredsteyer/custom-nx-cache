aa4713a53393158c2c8c526e550d9ad1fce03430
Manfred Steyer <manfred.steyer@gmx.net>
2021-02-15 16:50:10 +0100

    init

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/flight-app',
+    '<rootDir>/apps/dashboard',
+    '<rootDir>/apps/flight-admin',
+    '<rootDir>/libs/logger-lib',
+    '<rootDir>/libs/flight-lib',
+    '<rootDir>/libs/dashboard-lib',
+    '<rootDir>/libs/luggage/domain',
+    '<rootDir>/apps/luggage',
+    '<rootDir>/libs/luggage/feature-checkin',
+    '<rootDir>/libs/luggage/feature-report-loss',
+  ],
+};
