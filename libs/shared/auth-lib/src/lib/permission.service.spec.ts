ae85b6adcc461080357ef245fd16e87218fe827e
Manfred Steyer <manfred.steyer@gmx.net>
2021-02-15 17:22:57 +0100

    add jest test

---
+++ libs/shared/auth-lib/src/lib/permission.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+
+import { PermissionService } from './permission.service';
+
+describe('PermissionService', () => {
+  let service: PermissionService;
+
+  beforeEach(() => {
+    // TestBed.configureTestingModule({});
+    // service = TestBed.inject(PermissionService);
+  });
+
+  // it('has permission', waitForAsync(() => {
+    
+  //   service.hasPermission('flyThePlane').subscribe(result => {
+  //     expect(result).toBe(false);
+  //   })
+
+  // }));
+});
